6f73ffe42cea642b6aa57221bec5f62438be3a2d
annam21 <anna.moeller@umontana.edu>
2017-09-06 17:43:50 -0600

    More words
    Added words

--- wordcloud.R
+++ wordcloud.R
@@ -2,9 +2,11 @@
 
   dat <- tibble(
     name = "Anna", 
-    word = c("unmarked", "abundance", "sampling", "demographics", "time to event", 
-             "distribution", "cameras", "population", "elk", "mule deer" ),
-    importance = c(10, 10, 10, 10, 8, 6, 5, 7, 4, 2)
+    word = c("unmarked", "abundance", "sampling", "demographics", "time-to-event", 
+             "distribution", "cameras", "population", "elk", "mule deer",
+             "Idaho", "R", "Bayesian", "likelihood", "quantitative", "space-to-event",
+             "modeling", "survival", "spatiotemporal"),
+    importance = c(10, 10, 10, 10, 8, 6, 5, 6, 3, 1, 1, 3, 4, 6, 5, 6, 4, 4, 4)
     )
   
   save(dat, file = "GitHub/LukacsLabWordCloud/data/moeller.RData")
